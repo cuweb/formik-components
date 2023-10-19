e83689090bc2c507274eb547c26ef848ed97897e
Ish <ishdeep.711@gmail.com>
2023-10-19 16:30:15 -0400

    change: adding the carleton rds to the forms

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,39 @@
 // src/components/Home.js
 import React from "react";
 import Page from "./Page";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <Page>
       <div>
-        <h1>Welcome to the RDS Forms project </h1>
-        <p>need to add more content and link to the various forms.</p>
+        <h1 className="text-2xl font-bold leading-7">
+          Welcome to the RDS Forms project{" "}
+        </h1>
+
+        <ul className="justify-center font-medium mb-10 ">
+          <li className="hover:text-red-500">
+            <Link to="/formExample">Form Example</Link>
+          </li>
+        </ul>
+        <h2 className="text-xl font-bold leading-7">Job Forms example forms</h2>
+        <ul className="justify-center font-medium ">
+          <li className="hover:text-red-500">
+            <Link to="/casualJobs">Casual Jobs</Link>
+          </li>
+          <li className="hover:text-red-500">
+            <Link to="/jobAction">Job Action</Link>
+          </li>
+          <li className="hover:text-red-500">
+            <Link to="/employeeApproval">Employee Approval</Link>
+          </li>
+          <li className="hover:text-red-500">
+            <Link to="/payrollServices">Payroll Services</Link>
+          </li>
+          <li className="hover:text-red-500">
+            <Link to="/researchFinancial">Research Financial</Link>
+          </li>
+        </ul>
       </div>
     </Page>
   );
